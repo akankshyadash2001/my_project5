b3f35bb969c362fb4bba20397116cc44831dc71f
akankshyadash2001 <akankshyadash2001.com>
2023-12-11 12:12:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+def aku(request):
+    return render(request,'aku.html')
+
+
+
+
+# Create your views here.
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+def twin(request):
+    return render(request,'twin.html')
+
+# Create your views here.
